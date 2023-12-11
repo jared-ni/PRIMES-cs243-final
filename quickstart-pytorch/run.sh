4564cf9ffb9555b84a97d20eeddb718061699cd5
Jared Ni <milkteadj@gmail.com>
2023-12-10 23:20:24 -0500

    experiment 1 complete

--- quickstart-pytorch/run.sh
+++ quickstart-pytorch/run.sh
@@ -9,20 +9,32 @@ echo "Starting server"
 python server.py &
 sleep 5  # Sleep for 3s to give the server enough time to start
 
-for i in `seq 0 10`; do
+for i in `seq 0 9`; do
     echo "Starting client $i"
-    python client.py --fraction 15 --corruption 0.9 &
+    python client.py --fraction 50 --corruption 0.9 &
 done
 
-for i in `seq 11 13`; do
+for i in `seq 10 19`; do
     echo "Starting client $i"
-    python client.py --fraction 15 --corruption 0.1 &
+    python client.py --fraction 50 --corruption 0.7 &
 done
 
-# for i in `seq `; do
-#     echo "Starting client $i"
-#     python client.py --fraction 1000 &
-# done
+for i in `seq 20 29`; do
+    echo "Starting client $i"
+    python client.py --fraction 50 --corruption 0.5 &
+done
+
+for i in `seq 30 39`; do
+    echo "Starting client $i"
+    python client.py --fraction 50 --corruption 0.3 &
+done
+
+for i in `seq 40 49`; do
+    echo "Starting client $i"
+    python client.py --fraction 50 --corruption 0.1 &
+done
+
+
 
 # Enable CTRL+C to stop all background processes
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM
--- quickstart-pytorch/server.py
+++ quickstart-pytorch/server.py
@@ -38,7 +38,7 @@ _trainloaders, _validationloaders, testloader = prepare_dataset(
 
 strategy = PrimesStrategy(
     fraction_fit=0.2,  # in simulation, since all clients are available at all times, we can just use `min_fit_clients` to control exactly how many clients we want to involve during fit
-    min_fit_clients=3,  # number of clients to sample for fit()
+    min_fit_clients=10,  # number of clients to sample for fit()
     fraction_evaluate=1.0,  # similar to fraction_fit, we don't need to use this argument.
     min_evaluate_clients=3,  # number of clients to sample for evaluate()
     min_available_clients=3,  # total clients in the simulation
